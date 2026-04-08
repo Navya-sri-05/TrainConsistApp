3b117176c8cf7d492becd2daa0f377aa0ffc4164
Navya-sri-05 <navyasri3216@gmail.com>
2026-04-08 14:02:23 +0530

    UC1: Initialize Train and Display Summary

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistApp {
-}
+
+
+
+
+        public static void main(String[] args) {
+            // 1. Print a welcome message
+            System.out.println("=== Train Consist Management App ===");
+
+            // 2. Initialize an empty List using ArrayList to store bogies dynamically
+            // We use the List interface for abstraction
+            List<String> trainConsist = new ArrayList<>();
+
+            System.out.println("\nTrain initialized successfully...");
+
+            // 3. Display the initial bogie count using size() [cite: 1]
+            System.out.println("Initial Bogie Count : " + trainConsist.size());
+
+            // 4. Display the current state of the consist [cite: 1]
+            System.out.println("Current Train Consist : " + trainConsist);
+        }
+    }
+

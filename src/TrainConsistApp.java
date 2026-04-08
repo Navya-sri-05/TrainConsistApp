52bff50d6b4da4b5e62286a7cd80bf5ea9775c30
Navya-sri-05 <navyasri3216@gmail.com>
2026-04-08 14:56:33 +0530

    UC16: Manual sorting using Bubble Sort

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -1,42 +1,42 @@
 public class TrainConsistApp {
 
-    // Custom Runtime Exception
-    static class CargoSafetyException extends RuntimeException {
-        public CargoSafetyException(String message) {
-            super(message);
-        }
-    }
-
-    // Method to assign cargo
-    public static void assignCargo(String shape, String cargo) {
-
-        try {
-            // Unsafe condition
-            if (shape.equals("Rectangular") && cargo.equals("Petroleum")) {
-                throw new CargoSafetyException("Unsafe cargo assignment!");
-            }
-
-            // Safe case
-            System.out.println("Cargo assigned successfully -> " + cargo);
-
-        } catch (CargoSafetyException e) {
-            System.out.println("Error: " + e.getMessage());
-        } finally {
-            System.out.println("Cargo validation completed for " + shape + " bogie\n");
-        }
-    }
-
     public static void main(String[] args) {
 
-        System.out.println("UC15 - Safe Cargo Assignment");
-        System.out.println("=================================\n");
+        System.out.println("UC16 - Manual Sorting using Bubble Sort");
+        System.out.println("========================================\n");
 
-        // Safe case
-        assignCargo("Cylindrical", "Petroleum");
+        // Step 1: Create array
+        int[] capacities = {72, 56, 24, 70, 60};
 
-        // Unsafe case
-        assignCargo("Rectangular", "Petroleum");
+        // Step 2: Print original
+        System.out.println("Original Capacities:");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
 
-        System.out.println("UC15 runtime handling completed...");
+        System.out.println("\n");
+
+        // Step 3: Bubble Sort
+        int n = capacities.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+
+                if (capacities[j] > capacities[j + 1]) {
+                    // swap
+                    int temp = capacities[j];
+                    capacities[j] = capacities[j + 1];
+                    capacities[j + 1] = temp;
+                }
+            }
+        }
+
+        // Step 4: Print sorted
+        System.out.println("Sorted Capacities (Ascending):");
+        for (int c : capacities) {
+            System.out.print(c + " ");
+        }
+
+        System.out.println("\n\nUC16 sorting completed...");
     }
 }
